29086c51dc2d9c2aafaf8c1c9dd4f5c44ecbaa39
NunesNathan <nunesxd7@hotmail.com>
2021-11-11 19:42:23 -0300

    :white_check_mark: C3 - Requisito 2

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router';
-import { Link } from 'react-router-dom';
 import Album from '../pages/Album';
 import Favorites from '../pages/Favorites';
 import Login from '../pages/Login';
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,34 +1,76 @@
 import React, { Component } from 'react';
-// import createUser from '../services/userAPI';
-const { createUser } = require('../services/userAPI');
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Search from './Search';
 
 export default class Login extends Component {
-  loginValidate = (e) => {
-    const { value } = e.target.previousSibling;
-    const minCharac = 3;
-    console.log(value.length >= minCharac);
-    return value.length >= minCharac
-      ? createUser({ name: value })
-      : null;
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      isEnabled: true,
+      toLoading: false,
+      toGo: false,
+    };
   }
 
+  test = (name) => {
+    this.setState({
+      toLoading: true,
+    }, () => createUser({ name })
+      .then(() => this.setState({
+        toGo: true,
+      })));
+  }
+
+  buttonValidate = (e) => {
+    const min = 3;
+    const { value } = e.target;
+    const length = (value.length >= min);
+    this.setState({
+      name: value,
+    }, () => {
+      this.setState({
+        isEnabled: !length,
+      });
+    });
+  };
+
   render() {
+    const { name, toLoading, isEnabled, toGo } = this.state;
     return (
       <div data-testid="page-login">
         <h2>Login</h2>
         <form>
           <label htmlFor="login">
-            <input placeholder="Login" id="login" data-testid="login-name-input" />
+            <input
+              placeholder="Login"
+              data-testid="login-name-input"
+              onChange={ this.buttonValidate }
+              id="login"
+            />
             <button
               data-testid="login-submit-button"
               id="login"
               type="button"
-              onClick={ this.loginValidate }
+              disabled={ isEnabled }
+              onClick={
+                () => this.test(name)
+              }
             >
               Entrar
             </button>
           </label>
         </form>
+        {
+          toLoading
+          && <h3>Carregando...</h3>
+        }
+        {
+          toGo
+          && <Redirect to="/search" component={ Search } />
+        }
       </div>
     );
   }
