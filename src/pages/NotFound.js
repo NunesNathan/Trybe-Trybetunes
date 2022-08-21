14c7e207f8c573ebac16f00071f325f0660c7bd0
NunesNathan <nunesxd7@hotmail.com>
2022-08-21 20:52:56 -0300

    feat: not found page structure

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -4,7 +4,20 @@ export default class NotFound extends Component {
   render() {
     return (
       <div data-testid="page-not-found">
-        <h2>NotFound</h2>
+        <h1>Not Found</h1>
+        <p>Parece que você está sem sintonia! =/</p>
+        <a
+          data-testid="link-to-search"
+          href="/"
+        >
+          Voltar ao Login
+        </a>
+        <a
+          href="/search"
+          data-testid="link-to-search"
+        >
+          Ir à Home
+        </a>
       </div>
     );
   }
